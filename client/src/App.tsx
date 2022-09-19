53c8f2b3f9ceea18fc1938fe651091834ff32b6b
Thiago Miarelli <46660124+thiagomiarelli@users.noreply.github.com>
2022-09-19 16:14:58 -0300

    Project Setup

---
+++ client/src/App.tsx
@@ -0,0 +1,10 @@
+import {Button} from './Button'
+
+function App() {
+
+  return (
+    <Button title="Chama bb"/>
+  )
+}
+
+export default App
---
+++ client/src/Button/index.tsx
@@ -0,0 +1,8 @@
+interface ButtonProps {
+    title: string;
+}
+
+export function Button({title}: ButtonProps) {
+  return <button>{title}</button>
+}
+
