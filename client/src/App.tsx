4ce71a3af652ff89abc162ec36642e20a4e3b3fb
bernborgess <bernborgess@outlook.com>
2022-09-24 13:42:26 -0300

    fix:theme do MUI

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,44 @@
 // pages
-import Dashboard
-  from "./pages/Dashboard/Dashboard"
-import Login from './pages/Login/Login'
+import Dashboard from "./pages/Dashboard/Dashboard";
+import Login from './pages/Login/Login';
+
+declare module '@mui/material/styles' {
+  interface Theme {
+    status: {
+      danger: string;
+    };
+  }
+  // allow configuration using `createTheme`
+  interface ThemeOptions {
+    status?: {
+      danger?: string;
+    };
+  }
+}
+
+import { green, purple } from '@mui/material/colors';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: purple[500],
+    },
+    secondary: {
+      main: green[500],
+    },
+  },
+});
+
+
 
 function App() {
 
   return (
-    <>
-      <Dashboard/>
-      <Login/>
-    </>
+    <ThemeProvider theme={theme}>
+      <Dashboard />
+      <Login />
+    </ThemeProvider>
   )
 }
 
