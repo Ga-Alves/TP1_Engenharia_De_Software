743261a498019ae4e2aeb5b0560f26fb7a330325
bernborgess <bernborgess@outlook.com>
2022-09-26 17:54:48 -0300

    feat:login de usuario

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,10 +1,15 @@
-import { Router} from "express";
+import { Router } from "express";
 import { CreateStudentController } from "./controllers/CreateStudentController";
+import { LoginStudentController } from "./controllers/LoginStudentController";
 
 const router = Router();
 
-const createStudent= new CreateStudentController();
+const createStudent = new CreateStudentController();
+const loginStudentController = new LoginStudentController();
 
 router.post('/aluno/signin', createStudent.handle);
+router.post('/aluno/login', loginStudentController.handle);
+
 
 export { router };
+
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,30 @@
-import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
+import express, { NextFunction, Request, Response } from 'express';
+import "express-async-errors";
 import { router } from './routes';
+
+
+dotenv.config();
 
 const app = express();
 
 app.use(express.json());
+
 app.use(router);
 
-app.get('/test', (req: Request, res: Response) => {
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+  if (err instanceof Error) {
+    return response.status(400).json({
+      error: err.message
+    });
+  }
+  return response.status(500).json({
+    status: "error",
+    message: "Internal Server Error"
+  });
+})
+
+app.get('/', (req: Request, res: Response) => {
   console.log("Isso e um test!");
   return res.json({
     text: "text",
@@ -14,6 +32,6 @@ app.get('/test', (req: Request, res: Response) => {
   });
 })
 
-app.listen(3333, () => {
-  console.log("Its alive on port 3333")
+app.listen(process.env.BACKEND_PORT, () => {
+  console.log(`Its alive on port ${process.env.BACKEND_PORT}`)
 });
