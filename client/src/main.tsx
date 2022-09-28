8ffc8b1f7e0440fc968cc3b8a40fc844818ef5ab
Ga-Alves <gabrielalvesreis@gmail.com>
2022-09-27 23:22:55 -0300

    feat: Card grid dashboard

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,9 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
+// style
+import './index.css'
+
+// pages
 import App from './App'
+import Dashboard
+  from './pages/Dashboard/Dashboard';
+import Login
+  from './pages/Login/Login';
+import List
+ from './pages/Dashboard/SubjectGrid/SubjectGrid';
+
+import {BrowserRouter, Routes, Route}from 'react-router-dom';
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App />}>
+          <Route path='dashboard' element={<Dashboard />}>
+            <Route path='list' element={<List />}/>
+          </Route>
+          <Route path='login' element={<Login />}>
+          </Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+    
   </React.StrictMode>
 )
