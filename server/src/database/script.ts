ccada027771a6e643aa545519cf9a71c564c2316
bernborgess <bernborgess@outlook.com>
2022-09-29 20:06:02 -0300

    feat: definir no banco as materias

--- server/src/database/script.ts
+++ server/src/database/script.ts
@@ -1,9 +1,30 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, Subject } from '@prisma/client';
 
 export const prismaClient = new PrismaClient();
 
 
 async function main() {
+  //? Valores das materias
+  const { _count: { id: subjectCount } } =
+    await prismaClient.subject.aggregate({
+      _count: { id: true }
+    });
+
+  if (subjectCount === 0) {
+    const defaultSubjects: Pick<Subject, 'name' | 'syllabus'>[] = [
+      { name: "Matematica", syllabus: "Fazer conta de +" },
+      { name: "Portugues", syllabus: "Escrever serto" }
+    ];
+
+    for (let data of defaultSubjects)
+      await prismaClient.subject.create({ data });
+
+  }
+
+
+  const allSubjects = await prismaClient.subject.findMany();
+  console.log(allSubjects);
+
 }
 
 main()
