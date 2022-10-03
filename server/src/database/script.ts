a1dd39f1ac0d0929cc28920c5a78b2f23c3d345e
bernborgess <bernborgess@outlook.com>
2022-10-03 15:22:48 -0300

    feat:materias iniciais

--- server/src/database/script.ts
+++ server/src/database/script.ts
@@ -12,9 +12,24 @@ async function main() {
 
   if (subjectCount === 0) {
     const defaultSubjects: Pick<Subject, 'name' | 'syllabus'>[] = [
-      { name: "Matematica", syllabus: "Fazer conta de +" },
-      { name: "Portugues", syllabus: "Escrever serto" }
+      {
+        name: "Algoritmos 2",
+        syllabus: "Algoritmos para manipulação de sequências. Teoria de NP-completude e técnicas de solução exata e aproximada de problemas NP-difíceis. Algoritmos para dados geométricos"
+      },
+      {
+        name: "Pesquisa Operacional",
+        syllabus: "Modelagem. Fundamentos de programação matemática. Programação linear e suas aplicações. Método simplex. Análise de sensibilidade e dualidade. Otimização em redes. Programação dinâmica."
+      },
+      {
+        name: "Linguagens de Programação",
+        syllabus: "Linguagens de programação procedimentais e aplicativas. Estudo detalhado de algumas linguagens importantes. Critérios e técnicas na especificação e projeto de linguagens."
+      },
+      {
+        name: "Equações Diferenciais C",
+        syllabus: "Equações Diferenciais Ordinárias de 1a e 2a Ordens - Soluções de Equações Diferenciais em Séries de Potências - Sistemas de Equações Diferenciais Lineares - Transformada de Laplace - Séries de Fourier - Equações Diferenciais Parciais"
+      },
     ];
+
 
     for (let data of defaultSubjects)
       await prismaClient.subject.create({ data });
