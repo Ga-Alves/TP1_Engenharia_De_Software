20775b8cf2811598e0e2680a887cc1625c3cfd44
bernborgess <bernborgess@outlook.com>
2022-10-05 16:49:08 -0300

    create Evaluation

---
+++ server/src/middlewares/CreateEvaluationValidator.ts
@@ -0,0 +1,8 @@
+import { body } from 'express-validator';
+
+function createEvaluationValidator() {
+  body('rating').isInt({ min: 1, max: 5 });
+  body('difficulty').isInt({ min: 1, max: 5 });
+  body('recommended').isBoolean();
+  body('evaluation_method').isAlpha('pt-BR', { ignore: '' });
+}
