d3d4a23c4ccb37630823c1ab87404b0cfb90ddd9
bernborgess <bernborgess@outlook.com>
2022-10-19 17:07:49 -0300

    ref: arquivos de controller

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,42 +1,34 @@
 import { Router } from "express";
-import { CreateEvaluationController } from "./controllers/CreateEvaluationController";
-import { CreateStudentController } from "./controllers/CreateStudentController";
-import { GetEvaluationsBySubjectIdController } from "./controllers/GetEvaluationsBySubjectIdController";
-import { GetProfessorListBySubjectIdController } from "./controllers/GetProfessorListBySubjectIdController";
-import { GetProfessorListController } from "./controllers/GetProfessorListController";
-import { GetStudentController } from "./controllers/GetStudentController";
+import { EvaluationController } from "./controllers/EvaluationController";
 import { GetSubjectByIdController } from "./controllers/GetSubjectByIdController";
 import { GetSubjectController } from "./controllers/GetSubjectController";
-import { LoginStudentController } from "./controllers/LoginStudentController";
+import { ProfessorController } from "./controllers/ProfessorController";
+import { StudentController } from "./controllers/StudentController";
 import { studentValidate } from "./middlewares/CreateEvaluationValidator";
 
 const router = Router();
 
-const createStudent = new CreateStudentController();
-const loginStudentController = new LoginStudentController();
-const getStudentController = new GetStudentController();
+const studentController = new StudentController();
 
 const getSubjectController = new GetSubjectController();
 const getSubjectByIdController = new GetSubjectByIdController();
 
-const getEvaluationsBySubjectIdController = new GetEvaluationsBySubjectIdController();
-const createEvaluationController = new CreateEvaluationController();
+const evaluationController = new EvaluationController();
 
-const getProfessorListController = new GetProfessorListController();
-const getProfessorListBySubjectIdController = new GetProfessorListBySubjectIdController();
+const professorController = new ProfessorController();
 
-router.post('/aluno/signup', createStudent.handle);
-router.post('/aluno/login', loginStudentController.handle);
-router.get('/aluno/:id', getStudentController.handle);
+router.post('/aluno/signup', studentController.handleCreateStudent);
+router.post('/aluno/login', studentController.handleLoginStudent);
+router.get('/aluno/:id', studentController.handleGetStudent);
 
 router.get('/materia', getSubjectController.handle);
 router.get('/materia/:id', getSubjectByIdController.handle);
 
-router.get('/avaliacoes/:id', getEvaluationsBySubjectIdController.handle);
-router.post('/avaliacoes', studentValidate(), createEvaluationController.handle);
+router.get('/avaliacoes/:id', evaluationController.handleGetEvaluationBySubjectId);
+router.post('/avaliacoes', studentValidate(), evaluationController.handleCreateEvaluation);
 
-router.get('/professores', getProfessorListController.handle);
-router.get('/professores/materia/:id', getProfessorListBySubjectIdController.handle);
+router.get('/professores', professorController.handleGetProfessors);
+router.get('/professores/materia/:id', professorController.handleGetProfessorsBySubjectId);
 
 
 export { router };
