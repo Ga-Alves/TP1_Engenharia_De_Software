5e21a605374adbec832b8fcc42b866b45f484ba4
bernborgess <bernborgess@outlook.com>
2022-10-20 02:00:09 +0000

    fix: rotas back

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,17 +1,15 @@
 import { Router } from "express";
 import { EvaluationController } from "./controllers/EvaluationController";
-import { GetSubjectByIdController } from "./controllers/GetSubjectByIdController";
-import { GetSubjectController } from "./controllers/GetSubjectController";
 import { ProfessorController } from "./controllers/ProfessorController";
 import { StudentController } from "./controllers/StudentController";
+import { SubjectController } from "./controllers/SubjectController";
 import { studentValidate } from "./middlewares/CreateEvaluationValidator";
 
 const router = Router();
 
 const studentController = new StudentController();
 
-const getSubjectController = new GetSubjectController();
-const getSubjectByIdController = new GetSubjectByIdController();
+const subjectController = new SubjectController();
 
 const evaluationController = new EvaluationController();
 
@@ -21,8 +19,8 @@ router.post('/aluno/signup', studentController.handleCreateStudent);
 router.post('/aluno/login', studentController.handleLoginStudent);
 router.get('/aluno/:id', studentController.handleGetStudent);
 
-router.get('/materia', getSubjectController.handle);
-router.get('/materia/:id', getSubjectByIdController.handle);
+router.get('/materia', subjectController.handleGetSubjects);
+router.get('/materia/:id', subjectController.handleGetSubjectById);
 
 router.get('/avaliacoes/:id', evaluationController.handleGetEvaluationBySubjectId);
 router.post('/avaliacoes', studentValidate(), evaluationController.handleCreateEvaluation);
